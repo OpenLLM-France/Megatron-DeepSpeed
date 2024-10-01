34d15eb0745ae182263bc960d1fdd1d7c1216bc6
Oligou <oliviergouvert@gmail.com>
2024-10-01 16:57:18 +0200

    Add device and iteration in file saved

--- megatron/training.py
+++ megatron/training.py
@@ -1292,6 +1292,7 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
 
     return iteration
 
+idx_for_saving = 0
 def evaluate(forward_step_func,
              data_iterator,
              model,
@@ -1336,7 +1337,12 @@ def evaluate(forward_step_func,
                 # DeepSpeed uses eval_batch() and already aggregates losses.
                 assert isinstance(model, list) and len(model) == 1
                 loss, logits = model[0].eval_batch(data_iterator, return_logits=True)
-                torch.save(logits.to('cpu'), f'/lustre/fsn1/projects/rech/qgz/uzq54wg/out_logits_1024/logits_rank{logits.get_device()}_{iteration:04d}.pt')
+                global idx_for_saving
+                if logits is not None:
+                    torch.save(logits.to('cpu'), f'/lustre/fsn1/projects/rech/qgz/uzq54wg/out_logits_1024/logits_rank{torch.distributed.get_rank()}_device{logits.get_device()}_iteration{iteration}_global{idx_for_saving}.pt')
+                else:
+                    print(f'Rank: {torch.distributed.get_rank()}; global index: {idx_for_saving:04d} -> logits is None')
+                idx_for_saving += 1
                 loss_dicts = [{'lm loss' : loss}] * get_num_microbatches()
             else:
                 loss_dicts = forward_backward_func(
