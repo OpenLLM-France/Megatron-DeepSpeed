7cb755c0ad71df9fdd71213933ba1024511d107e
Oligou <oliviergouvert@gmail.com>
2024-10-07 17:08:57 +0200

    save .safetensors, bf16, max 5GB

--- tools/convert_checkpoint/universal_to_hf_llama.py
+++ tools/convert_checkpoint/universal_to_hf_llama.py
@@ -1,9 +1,10 @@
 import argparse
-from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaConfig
-import os
+from transformers import LlamaConfig
 import torch
 import json
-from transformers.modeling_utils import WEIGHTS_INDEX_NAME, WEIGHTS_NAME, shard_checkpoint
+import os
+from safetensors.torch import save_file as safe_save_file
+from huggingface_hub import split_torch_state_dict_into_shards
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -85,45 +86,45 @@ def main():
     output_state_dict = {}
 
     # Embedding
-    embedding_weight = torch.load(os.path.join(zero_path, "1.word_embeddings.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+    embedding_weight = torch.load(os.path.join(zero_path, "1.word_embeddings.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
     output_state_dict["model.embed_tokens.weight"] = embedding_weight[: config.vocab_size, :]
 
     # Final Layer
-    finalnorm_weight = torch.load(os.path.join(zero_path, f"{num_layers+2}.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
-    lm_head_weight = torch.load(os.path.join(zero_path, f"{num_layers+3}.lm_head.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+    finalnorm_weight = torch.load(os.path.join(zero_path, f"{num_layers+2}.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
+    lm_head_weight = torch.load(os.path.join(zero_path, f"{num_layers+3}.lm_head.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
     output_state_dict["model.norm.weight"] = finalnorm_weight
     output_state_dict["lm_head.weight"] = lm_head_weight
 
     print("Converting to HF Checkpoint")
     for l in range(num_layers):
         # Layer norm
-        input_layernorm = torch.load(os.path.join(zero_path, f"{l+2}.input_layernorm.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        input_layernorm = torch.load(os.path.join(zero_path, f"{l+2}.input_layernorm.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         output_state_dict[f"model.layers.{l}.input_layernorm.weight"] = input_layernorm
 
-        post_attention_layernorm = torch.load(os.path.join(zero_path, f"{l+2}.post_attention_layernorm.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        post_attention_layernorm = torch.load(os.path.join(zero_path, f"{l+2}.post_attention_layernorm.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         output_state_dict[f"model.layers.{l}.post_attention_layernorm.weight"] = post_attention_layernorm
 
         # MLP
-        dense_4h_to_h = torch.load(os.path.join(zero_path, f"{l+2}.mlp.dense_4h_to_h.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        dense_4h_to_h = torch.load(os.path.join(zero_path, f"{l+2}.mlp.dense_4h_to_h.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         output_state_dict[f"model.layers.{l}.mlp.down_proj.weight"] = dense_4h_to_h
 
-        dense_h_to_4h = torch.load(os.path.join(zero_path, f"{l+2}.mlp.dense_h_to_4h.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        dense_h_to_4h = torch.load(os.path.join(zero_path, f"{l+2}.mlp.dense_h_to_4h.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         gate_proj, up_proj = dense_h_to_4h.chunk(2, dim=0)
         output_state_dict[f"model.layers.{l}.mlp.gate_proj.weight"] = gate_proj
         output_state_dict[f"model.layers.{l}.mlp.up_proj.weight"] = up_proj
 
         # Attention
-        query = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.query.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        query = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.query.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         q_proj = fix_query_key_value_ordering(query, checkpoint_version, 1, num_attention_heads, hidden_size_per_head)
         output_state_dict[f"model.layers.{l}.self_attn.q_proj.weight"] = q_proj
 
-        key_value = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.key_value.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        key_value = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.key_value.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         key_value = fix_query_key_value_ordering(key_value, checkpoint_version, 2, num_key_value_heads, hidden_size_per_head)
         k_proj, v_proj = key_value.chunk(2, dim=0)
         output_state_dict[f"model.layers.{l}.self_attn.k_proj.weight"] = k_proj
         output_state_dict[f"model.layers.{l}.self_attn.v_proj.weight"] = v_proj
                 
-        dense = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.dense.weight/fp32.pt"), map_location=torch.device('cpu'))['param']
+        dense = torch.load(os.path.join(zero_path, f"{l+2}.self_attention.dense.weight/fp32.pt"), map_location=torch.device('cpu'))['param'].bfloat16()
         output_state_dict[f"model.layers.{l}.self_attn.o_proj.weight"] = dense
 
         # Rotary
@@ -141,32 +142,24 @@ def main():
     with open(output_config_file, "w") as f:
         json.dump(output_config, f)
 
-    # Store the state_dict to file.
-    # output_checkpoint_file = os.path.join(args.output_folder, "pytorch_model.bin")
-    # print(f'Saving checkpoint to "{output_checkpoint_file}"')
-    # torch.save(output_state_dict, output_checkpoint_file)
+    def save_state_dict(state_dict, save_directory):
+        state_dict_split = split_torch_state_dict_into_shards(state_dict)
+        for filename, tensors in state_dict_split.filename_to_tensors.items():
+            shard = {tensor: state_dict[tensor] for tensor in tensors}
+            safe_save_file(
+                shard,
+                os.path.join(save_directory, filename),
+                metadata={"format": "pt"},
+            )
+        if state_dict_split.is_sharded:
+            index = {
+                "metadata": state_dict_split.metadata,
+                "weight_map": state_dict_split.tensor_to_filename,
+            }
+            with open(os.path.join(save_directory, "model.safetensors.index.json"), "w") as f:
+                f.write(json.dumps(index, indent=2))
 
-    # Store the state_dict to file.
-    max_shard_size = int(args.max_shard_size) if args.max_shard_size.isdigit() else args.max_shard_size
-    shards, index = shard_checkpoint(output_state_dict, max_shard_size=max_shard_size)
-
-    # Save the model
-    for shard_file, shard in shards.items():
-        torch.save(shard, os.path.join(args.output_folder, shard_file))
-
-    if index is None:
-        print(f"Model weights saved in {os.path.join(args.output_folder, WEIGHTS_NAME)}")
-    else:
-        save_index_file = os.path.join(args.output_folder, WEIGHTS_INDEX_NAME)
-        # Save the index as well
-        with open(save_index_file, "w", encoding="utf-8") as f:
-            content = json.dumps(index, indent=2, sort_keys=True) + "\n"
-            f.write(content)
-        print(
-            f"The model is bigger than the maximum size per checkpoint ({args.max_shard_size}) and is going to be "
-            f"split in {len(shards)} checkpoint shards. You can find where each parameters has been saved in the "
-            f"index located at {save_index_file}."
-        )
+    save_state_dict(output_state_dict, args.output_folder)
 
     print("Now add tokenizer files and upload to the hub")
 
